2dd426615f3c20b3f3d8395ffadd7e3abfb96b30
Ayanacyan <amitpacl@gmail.com>
2026-02-25 18:50:29 -0600

    Sin Cos Tan Calculator

---
+++ Trignometry.py
@@ -0,0 +1,7 @@
+import math
+angle=int(input("Enter the angle degrees: "))
+radians=math.radians(angle)
+
+print("\nSin of",angle,"is:",math.sin(radians))
+print("\nCos of",angle,"is:",math.cos(radians))
+print("\nTan of",angle,"is:",math.tan(radians))
---
+++ math_operations.py
@@ -0,0 +1,8 @@
+import math
+print('The Floor and Ceiling Value of 23.5 are: '+str(math.ceil(23.56))+','+str(math.floor(23.56)))
+
+x=-88
+y=9
+print("The value of x after copying the sign from y is", str(math.copysign(x,y)))
+
+print("Absolute Value of -96 and 54 are: ", str(math.fabs(-96))+','+ str(math.fabs(54)))
